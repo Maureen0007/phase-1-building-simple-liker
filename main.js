3209ca36db430153ccfc7c7c18997863ebf3c2f9
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-19 00:44:47 +0300

    commit tree d7b9b44853016cb836e5549931d2c6ba1cfc42d0

--- main.js
+++ main.js
@@ -3,9 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likes = document.querySelectorAll('.like-glyph');
+const hiddenError =  document.querySelector('#modal');
+const modalMessage = document.querySelector('#modal-message');
+hiddenError.classList.add('hidden');
+likes.forEach((like)=> like.addEventListener('click',()=>{
+  mimicServerCall()
+  .then(()=> toggleLike(like))
+  .catch((error)=> display(error));
+}));
 
+function toggleLike(like){
+  if(like.textContent === EMPTY_HEART){
+    like.textContent = FULL_HEART;
+    like.style.color= "red";
+  }
+  else{
+    like.textContent = EMPTY_HEART;
+  }
+}
 
-
+function display(errorMessage){
+    hiddenError.classList.remove('hidden');
+    modalMessage.textContent = errorMessage;
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
